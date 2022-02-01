8593958590dd1a0a225b7154db71c1dc00b6443b
giulia notaro <notarogiulia10@gmail.com>
2022-02-01 19:39:48 +0100

    in progress

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <div>
         <ul className={styles.ul}>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/home">Home</Link>
           </li>
           <li>
             <Link to="/dispensa">Dispensa</Link>
@@ -22,7 +22,7 @@ function App() {
       </div>
 
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/dispensa" element={<Dispensa />} />
         <Route path="/listaspesa" element={<ListaSpesa />} />
       </Routes>
